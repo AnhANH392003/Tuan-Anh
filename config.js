9a82620df39f9f6afd5932e1d1a647aa7e484e7b
Tuấn ANh <93137135+AnhANH392003@users.noreply.github.com>
2021-11-07 17:11:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em cô bé đã từng là của anh';
+const nameGirl = 'hello';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 19-11';
+const eventName = 'Buổi Tối vui vẻ';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 19/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc bạn 1 buổi tối vui vẻ và 1 tuần mới sắp tới đầy may mắn';
 const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
